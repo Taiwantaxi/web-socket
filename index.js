5828165cf5640a18a68cd40ba5fa3d71614529b6
Bo-Rong <brown.huang@taiwantaxi.com.tw>
2017-11-17 16:31:36 +0800

    commit

--- index.js
+++ index.js
@@ -16,13 +16,60 @@ server.listen(PORT, function(){
   console.log('Listening on 3000');
 })
 
+var clients = [];
+
 io.on('connection', function(socket){
+
   console.log('a new client connected');
 
-  socket.on('message', function(data){
-    console.log('data:', data);
-    socket.emit('message','Server : Hello World');
+  socket.on('create_client',function(data){
+    console.log('create_client :', data);
+    
+    let username = data;
+    socket.name = username;
+    clients.push(socket);
+    socket.emit('create_client',`create_client ${username} is OK`);    
+
+  });
+
+  socket.on('send_message', function(data){
+    console.log('send_message :', data);
+    
+    let message = data;
+    sendMessageToClient(clients, socket, message);
   })
 
+  socket.on('disconnect',function(data){
+    console.log('disconnect :', data);    
+    deleteClient(clients, socket);
+  }); 
 
 });
+
+const sendMessageToClient = function(clients, socket, message){
+  
+    for(let i = 0; i < clients.length; i++){
+      let client = clients[i];
+      if(client === socket){
+        //Do Nothing
+        console.log(socket.name,' said :',message);      
+        
+      }else{
+        client.emit('send_message',`${socket.name} said : ${message}`);        
+      }
+    }
+  
+}
+
+const deleteClient = function(clients, socket){
+  
+    for(let i = 0; i < clients.length; i++){
+      let client = clients[i];
+      if(client === socket){
+        console.log('user', client.name, 'disconnet');
+        clients.splice(i, 1 );
+      }else{
+        //Do Nothing        
+      }
+  }
+}
